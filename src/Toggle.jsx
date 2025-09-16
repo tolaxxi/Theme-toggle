408b6d6840ae6171cd05dbead068ac7c4d873ee0
Tolani Onajoko <omotolanionajoko@gmail.com>
2025-09-16 18:58:34 +0100

    feat:add ui and functionality

--- src/Toggle.jsx
+++ src/Toggle.jsx
@@ -1,6 +1,26 @@
+import { useState } from 'react';
+import './toggle.css';
+
 const Toggle = () => {
+  const [theme, setTheme] = useState('white');
+  const [textColor, setTextColor] = useState('#000000');
+
+  function handleToggle() {
+    setTextColor(textColor === '#e10000' ? '#000000' : '#e10000');
+    setTheme(theme==='#1b1b1b'?"white":"#1b1b1b");
+  }
+
   return (
-    <div>Toggle</div>
-  )
-}
-export default Toggle
+    <section style={{ backgroundColor: theme }}>
+      <button
+        className="toggle-btn"
+        onClick={handleToggle}
+        style={{ border: `2px solid ${textColor}`, color: textColor }}
+      >
+        {theme === 'white' ? 'Dark Theme' : 'Light Theme'}
+      </button>
+      <h1 style={{ color: textColor }}>Mr. Robot</h1>
+    </section>
+  );
+};
+export default Toggle;
